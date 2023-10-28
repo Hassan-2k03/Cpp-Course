70f00b709de46ea4027f1c5fca6f859be2832541
Mohammed Hassan <mh1921544@gmail.com>
2023-10-28 12:49:46 +0530

    Changes to be committed: 	new file:   .gitignore 	new file:   Basics/add.cpp 	new file:   Basics/helloword.cpp 	new file:   Basics/pointers.cpp

---
+++ Basics/add.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    //user input
+    int a, b;
+    cout << "Enter two numbers: ";
+    cin >> a >> b;
+    
+    //output
+    cout << "Sum = " << a + b;
+    return 0;
+}
