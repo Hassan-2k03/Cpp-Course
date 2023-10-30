e4baa26e7947c63bb58472a02f660164ceaf0bc7
Mohammed Hassan <mh1921544@gmail.com>
2023-10-30 12:40:31 +0530

    Update pointers.cpp

--- Basics/pointers.cpp
+++ Basics/pointers.cpp
@@ -1,13 +1,6 @@
 #include <iostream>
 using namespace std;
 
-/**
- * This program demonstrates the use of pointers in C++.
- * It declares an integer variable 'a', initializes it to 10,
- * and then declares a pointer variable 'ptr' that points to the address of 'a'.
- * It then prints the value of 'a', the address of 'a', and the value pointed to by 'ptr'.
- */
-
 int main()
 {
     int a =10;
